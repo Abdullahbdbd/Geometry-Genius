e93a7cd155f490332ce6782d496b5f5a992dd335
Abdullahbdbd <abdullahmohammad262000@gmail.com>
2023-02-18 22:36:25 +0600

    Final

--- index.js
+++ index.js
@@ -4,12 +4,15 @@
 document.getElementById('btn-blog').addEventListener('click', function(){
     window.location.href = 'blog.html';
 })
-//-------------------------------------------
+
+
+// Hover
+
 
 function randomColor() {
     let color = [];
     for (let i = 0; i < 3; i++) {
-      color.push(Math.floor(Math.random() * 256));
+      color.push(Math.floor(Math.random() * 2000));
     }
     return 'rgb(' + color.join(', ') + ')';
   } 
@@ -18,8 +21,8 @@ function randomColor() {
     document.body.style.backgroundColor = randomColor();
   });
   
- 
-//---------------------------------------
+
+
 // Triangle
 
 
@@ -28,10 +31,15 @@ document.getElementById('btn-first').addEventListener('click', function () {
       
     
 sirial +=1;
+
     const firstInput = document.getElementById('first-input')
     const firstInputNumString = firstInput.value;
     const firstInputNum = parseFloat(firstInputNumString);
-
+//----------------------------------
+if(isNaN(firstInputNum) || firstInputNum < 1){
+    alert('Please provide a valid Number')  
+}
+//-----------------------------------
     const firstInput2 = document.getElementById('first-input2')
     const firstInput2NumString = firstInput2.value;
     const firstInput2Num = parseFloat(firstInput2NumString);
@@ -64,6 +72,11 @@ document.getElementById('btn-second').addEventListener('click', function () {
     const secondInput = document.getElementById('second-input')
     const secondInputNumString = secondInput.value;
     const secondInputNum = parseFloat(secondInputNumString);
+
+    if(isNaN(secondInputNum) || secondInputNum < 1){
+        alert('Please provide a valid Number')  
+    }
+
 
     const secondInput2 = document.getElementById('second-input2')
     const secondInput2NumString = secondInput2.value;
@@ -101,6 +114,11 @@ document.getElementById('btn-third').addEventListener('click', function () {
     const thirdInputNumString = thirdInput.value;
     const thirdInputNum = parseFloat(thirdInputNumString);
 
+
+    if(isNaN(thirdInputNum) || thirdInputNum < 1){
+        alert('Please provide a valid Number')  
+    }
+
     const thirdInput2 = document.getElementById('third-input2')
     const thirdInput2NumString = thirdInput2.value;
     const thirdInput2Num = parseFloat(thirdInput2NumString);
@@ -136,6 +154,10 @@ document.getElementById('btn-fourth').addEventListener('click', function () {
     const fourthInputNumString = fourthInput.value;
     const fourthInputNum = parseFloat(fourthInputNumString);
 
+    if(isNaN(fourthInputNum) || fourthInputNum < 1){
+        alert('Please provide a valid Number')  
+    }
+
     const fourthInput2 = document.getElementById('fourth-input2')
     const fourthInput2NumString = fourthInput2.value;
     const fourthInput2Num = parseFloat(fourthInput2NumString);
@@ -169,6 +191,10 @@ document.getElementById('btn-fifth').addEventListener('click', function () {
     const fifthInput = document.getElementById('fifth-input')
     const fifthInputNumString = fifthInput.value;
     const fifthInputNum = parseFloat(fifthInputNumString);
+
+    if(isNaN(fifthInputNum) || fifthInputNum < 1){
+        alert('Please provide a valid Number')  
+    }
 
     const fifthInput2 = document.getElementById('fifth-input2')
     const fifthInput2NumString = fifthInput2.value;
@@ -204,6 +230,10 @@ document.getElementById('btn-sixth').addEventListener('click', function () {
     const sixthInput = document.getElementById('sixth-input')
     const sixthInputNumString = sixthInput.value;
     const sixthInputNum = parseFloat(sixthInputNumString);
+
+    if(isNaN(sixthInputNum) || sixthInputNum < 1){
+        alert('Please provide a valid Number')  
+    }
 
     const sixthInput2 = document.getElementById('sixth-input2')
     const sixthInput2NumString = sixthInput2.value;
