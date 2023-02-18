21415931d3ccc89eae7bd2d38f0e2ada5350b677
Abdullahbdbd <abdullahmohammad262000@gmail.com>
2023-02-18 17:26:12 +0600

    Parallelogram

--- index.js
+++ index.js
@@ -67,3 +67,39 @@ document.getElementById('btn-second').addEventListener('click', function () {
     `
     tableContainer.appendChild(tr);
 })
+
+
+
+
+// Parallelogram
+
+
+document.getElementById('btn-third').addEventListener('click', function () {
+
+
+    const thirdInput = document.getElementById('third-input')
+    const thirdInputNumString = thirdInput.value;
+    const thirdInputNum = parseFloat(thirdInputNumString);
+
+    const thirdInput2 = document.getElementById('third-input2')
+    const thirdInput2NumString = thirdInput2.value;
+    const thirdInput2Num = parseFloat(thirdInput2NumString);
+
+    const totalInputNum = thirdInputNum * thirdInput2Num;
+    const totalInputNumFixed = totalInputNum.toFixed(2);
+    thirdInput.value = '';
+    thirdInput2.value = '';
+
+
+
+    const tableContainer = document.getElementById('table-container')
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+    
+    <td>${1}</td>
+    <td>${'Parallelogram'}</td>
+    <td>${totalInputNumFixed}cm<sup>2</sup></td>
+    <button class="btn btn-sm mt-3 bg-blue-500">Covert to m2</button>
+    `
+    tableContainer.appendChild(tr);
+})
