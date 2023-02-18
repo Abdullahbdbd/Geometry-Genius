95bd2d22edb58df08f85ac147204dff459eba25b
Abdullahbdbd <abdullahmohammad262000@gmail.com>
2023-02-18 17:18:11 +0600

    Rectangle

--- index.js
+++ index.js
@@ -3,27 +3,27 @@
 
 
 
-document.getElementById('btn-first').addEventListener('click', function(){
-    
-    
+document.getElementById('btn-first').addEventListener('click', function () {
+
+
     const firstInput = document.getElementById('first-input')
     const firstInputNumString = firstInput.value;
-    const firstInputNum =parseFloat(firstInputNumString);
+    const firstInputNum = parseFloat(firstInputNumString);
 
     const firstInput2 = document.getElementById('first-input2')
     const firstInput2NumString = firstInput2.value;
-    const firstInput2Num =parseFloat(firstInput2NumString);
+    const firstInput2Num = parseFloat(firstInput2NumString);
 
-    const totalInputNum =0.5 * firstInputNum * firstInput2Num;
+    const totalInputNum = 0.5 * firstInputNum * firstInput2Num;
     const totalInputNumFixed = totalInputNum.toFixed(2);
-       firstInput.value = '';
-       firstInput2.value = '';
+    firstInput.value = '';
+    firstInput2.value = '';
 
 
 
     const tableContainer = document.getElementById('table-container')
     const tr = document.createElement('tr')
-    tr.innerHTML=`
+    tr.innerHTML = `
     
     <td>${1}</td>
     <td>${'Triangle'}</td>
@@ -35,3 +35,35 @@ document.getElementById('btn-first').addEventListener('click', function(){
 
 
 
+// Rectangle
+
+
+document.getElementById('btn-second').addEventListener('click', function () {
+
+
+    const secondInput = document.getElementById('second-input')
+    const secondInputNumString = secondInput.value;
+    const secondInputNum = parseFloat(secondInputNumString);
+
+    const secondInput2 = document.getElementById('second-input2')
+    const secondInput2NumString = secondInput2.value;
+    const secondInput2Num = parseFloat(secondInput2NumString);
+
+    const totalInputNum = secondInputNum * secondInput2Num;
+    const totalInputNumFixed = totalInputNum.toFixed(2);
+    secondInput.value = '';
+    secondInput2.value = '';
+
+
+
+    const tableContainer = document.getElementById('table-container')
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+    
+    <td>${1}</td>
+    <td>${'Rectangle'}</td>
+    <td>${totalInputNumFixed}cm<sup>2</sup></td>
+    <button class="btn btn-sm mt-3 bg-blue-500">Covert to m2</button>
+    `
+    tableContainer.appendChild(tr);
+})
